8e2b3968150f0a5070bf7182b0a0badc4bad3b70
Jeremy427 <135190@swcsd.us>
2025-02-25 14:07:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Jeremy Spence, 1660, 2/25/25
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        ContactInfo contactInfo1 = new ContactInfo("Jammy Sparks","JammySparks123@gmail.com");
+        contactInfo1.display();
+        BusinessContactInfo contactInfo2 = new BusinessContactInfo("David Peckham","Davidpeckham@gmail.com","518-316-6457");
+        contactInfo2.display();
+
+
+    }
+}
